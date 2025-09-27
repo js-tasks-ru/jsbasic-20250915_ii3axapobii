925c09d5d890970125564d491b942eac9af8b0ea
Nikita Zakharov <ii3axapobii@gmail.com>
2025-09-27 14:11:50 +0300

    Выполнено задание по написанию функции факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n === 0){
+    return (result);
+  } else if (n > 0){
+    for (let i = 1; i <= n; i++){
+      result = result * i;
+    }
+    return (result);
+  }
 }
