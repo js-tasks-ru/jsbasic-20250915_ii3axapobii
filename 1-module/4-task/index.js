a8b212896e599164947fb09a66729cb9342bc338
Nikita Zakharov <ii3axapobii@gmail.com>
2025-10-05 23:46:08 +0300

    Выполнение задания module-1 task-4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  if(str.includes('1xbet') || str.includes('xxx')){
+    return true;
+  }
+  else return false
 }
+
+let a = '1xbeT' 
+let result = checkSpam(a);
+console.log(result)
