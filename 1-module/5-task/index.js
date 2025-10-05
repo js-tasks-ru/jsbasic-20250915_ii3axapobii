5da424e969ecf8440549ac3272be7bb83502d043
Nikita Zakharov <ii3axapobii@gmail.com>
2025-10-06 00:05:26 +0300

    Выполнение задания module-1 task-5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,15 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if(str.length > maxlength){
+    let result = str.slice(0, maxlength) + '…';
+    return result;
+  }
+  else return str;
 }
+
+let testStr = 'Вот, что мне хотелось бы сказать на эту тему';
+let testRsalt = truncate(testStr, 20);
+console.log(testRsalt);
+
+// let str = 'Вот, что мне хотелось бы сказать на эту тему';
+// let result = str.slice(0, 20) + '…';
+// console.log(result)
