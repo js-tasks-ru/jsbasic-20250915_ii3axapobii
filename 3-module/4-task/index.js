f60ebb17a818895eea88554a275474333e570bb6
Nikita Zakharov <ii3axapobii@gmail.com>
2025-10-11 01:17:30 +0300

    Выполнение задания module-3 task-5 + комментарии

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,6 +1,6 @@
 function showSalary(users, age) {
   return users
-    .filter(user => user.age <= age)
-    .map(user => `${user.name}, ${user.balance}`)
-    .join('\n')
+    .filter(user => user.age <= age) // фильтруем все объекты в массиве users по возрасту
+    .map(user => `${user.name}, ${user.balance}`) // преображаем каждый объект массива users в строку "name, balance"
+    .join('\n') // джойним массив строк "name, balance" в строку, чтобы каждая строка "name, balance" начиналась с новой строки
 }
