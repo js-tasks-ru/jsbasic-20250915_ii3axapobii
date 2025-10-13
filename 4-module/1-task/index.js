baa9bfb7eb730c18adc4a5cc76d5e90b38b83a6e
Nikita Zakharov <ii3axapobii@gmail.com>
2025-10-14 01:08:58 +0300

    module-4 task-1 - комментарии + корректировки

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,11 +1,10 @@
 function makeFriendsList(friends) {
-  let friendsStr = friends.map(item => `${item.firstName} ${item.lastName}`);
-  let friendsList = document.createElement('UL');
+  let friendsStr = friends.map(item => `${item.firstName} ${item.lastName}`); //преобразование маасива объектов к массиву строк
+  let friendsUl = document.createElement('UL'); //создание ненумерованного списка
+  document.body.appendChild(friendsUl); //добавление списка в DOM
   for (let friendItem of friendsStr){
-    let friendLi = document.createElement('LI');
-    friendLi.textContent = friendItem;
-    friendsList.appendChild(friendLi);
+    let friendLi = document.createElement('LI'); //создание позиции списка. Пока находится только в памяти и не выводится
+    friendLi.textContent = friendItem; //присвоение позиции списка значения элемента массива
+    friendsUl.appendChild(friendLi); //добавление позиции в список
   }
-  return friendsList;
-
 }
