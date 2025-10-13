a19fa59fa419cdba411dcdbe87332e7113e4d027
Nikita Zakharov <ii3axapobii@gmail.com>
2025-10-14 00:53:32 +0300

    Выполнение задания module-4 task-1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let friendsStr = friends.map(item => `${item.firstName} ${item.lastName}`);
+  let friendsList = document.createElement('UL');
+  for (let friendItem of friendsStr){
+    let friendLi = document.createElement('LI');
+    friendLi.textContent = friendItem;
+    friendsList.appendChild(friendLi);
+  }
+  return friendsList;
+
 }
