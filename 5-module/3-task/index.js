5e67903eef6867305d6c60b5576796b83655c8cb
Nikita Zakharov <ii3axapobii@gmail.com>
2025-10-29 23:25:22 +0300

    Выполнение заданий module-5 task2 и task-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let carousel = document.querySelector('.carousel__inner');
+  let carouselSlide = document.querySelector('.carousel__slide');
+  let slideCounter = 0;
+
+  arrowDisplay();
+  arrowLeft.addEventListener('click', leftSlide);
+  arrowRight.addEventListener('click', rightSlide);
+
+  function rightSlide() {
+    slideCounter++;
+    carousel.style.transform = `translateX(-${carouselSlide.offsetWidth * slideCounter}px)`;
+    arrowDisplay();
+  }
+
+  function leftSlide() {
+    slideCounter--;
+    carousel.style.transform = `translateX(${-carouselSlide.offsetWidth * slideCounter}px)`;
+    arrowDisplay();
+  }
+
+  function arrowDisplay() {
+    if (slideCounter === 0) {
+      arrowLeft.style.display = 'none';
+    } else if (slideCounter === 3) {
+      arrowRight.style.display = 'none'
+    } else {
+      arrowLeft.style.display = '';
+      arrowRight.style.display = '';
+    }
+  }
 }
