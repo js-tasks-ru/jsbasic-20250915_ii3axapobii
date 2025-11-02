10e4f3040f3d601a0b3fae1e7d7855576c40d337
Nikita Zakharov <ii3axapobii@gmail.com>
2025-11-02 03:22:12 +0300

    module-6 task-1 - реализован класс который создает таблицу и наполняет ее данными из массива объектов

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,40 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.employees = rows;
+    this.elem = this.render();
   }
+  render() {
+    this.elem = document.createElement('TABLE');
+    this.elemBody = document.createElement('TBODY');
+
+    this.elem.innerHTML = ` 
+        <thead>
+          <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>`;
+    
+    this.elemBody.innerHTML = this.employees
+      .map(({ name, age, salary, city }) =>
+        `<tr>
+            <td>${name}</td>
+            <td>${age}</td>
+            <td>${salary}</td>
+            <td>${city}</td>
+          </tr>`
+      )
+      .join('');
+
+    this.elem.append(this.elemBody);
+
+    return this.elem;
+  }
+
 }
