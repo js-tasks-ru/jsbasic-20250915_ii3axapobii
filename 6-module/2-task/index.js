ac30994405d28ee740e27de89ca7524d7290051b
Nikita Zakharov <ii3axapobii@gmail.com>
2025-11-06 23:37:28 +0300

    Выполнение задания module-6 task-2

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,27 @@
+import createElement from '/assets/lib/create-element.js'
+
 export default class ProductCard {
   constructor(product) {
+    this.card = product;
+    this.elem = this.render();
+  }
+
+  render() {
+    const card = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${this.card.image}" class="card__image" alt="product">
+          <span class="card__price">€${this.card.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.card.name}</div>
+            <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+              </button>
+        </div>
+      </div>
+    `)
+
+    return card;
   }
 }
